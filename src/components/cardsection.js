561ba88afc6d12f03bbce37a7f40bd6842d252f8
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-19 16:15:37 -0300

    html

---
+++ src/components/cardsection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Cardsection() {
+  return (
+    <div>
+        <div className="card">
+          <img src="../images/plants.png" alt="plant" />
+          <span>
+            Plants
+          </span>
+        </div>
+        <div className="card">
+          <img src="../images/books.jpg" alt="plant" />
+          <span>
+            Book
+          </span>
+        </div>
+    </div>
+  );
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Navbar(){
+    return (
+            <div >
+               <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Blog</li>
+                    <li>Services</li>
+                    <li>About me</li>
+                    <li>Contact</li>
+                </ul>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                    <li>Pinterest</li>
+                    <li>Tiktok</li>
+                </ul>
+               </nav>
+            </div>
+    )
+}
